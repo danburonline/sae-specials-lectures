5caf705beb29f1de1ba36ba6c9200cdb91d0779f
Daniel Burger <public@danielburger.online>
2023-11-19 00:47:46 +0100

    Remove part from bash script that we don't need anymore

--- .devcontainer/scripts/post_create.sh
+++ .devcontainer/scripts/post_create.sh
@@ -6,7 +6,3 @@ npm install -g bun@1.0.13
 # Install dependencies via Bun from the Bun lockfile
 # ? The Bun lockfile is a binary file that contains a list of all dependencies
 bun install
-
-# Install command line utilities
-sudo apt-get update &&
-  sudo apt-get install -y netcat # ? Netcat: For testing TCP connections
