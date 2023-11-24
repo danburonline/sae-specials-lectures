3d74a3a0311697bd41f347f4f1d886e769c286ec
Nadine Keller <nadine@MacBook-Pro-von-Nadine.local>
2023-11-24 16:06:44 +0100

    add search bar component

--- apps/web/src/app/components/ui/play.tsx
+++ apps/web/src/app/components/ui/play.tsx
@@ -9,7 +9,7 @@ export default function Play() {
   return (
     <Canvas shadows="variance" camera={{ position: [0, 0, 25], fov: 50 }} className="bg-gray-300">
       <Composition />
-      <Perf position="top-left" />
+      <Perf position="top-right" />
       <GizmoHelper alignment="bottom-right" margin={[80, 80]}>
         <GizmoViewport axisColors={['#9d4b4b', '#2f7f4f', '#3b5b9d']} labelColor="white" />
       </GizmoHelper>
--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,9 +1,23 @@
-'use client'
+/* eslint-disable simple-import-sort/imports */
+// PlayPage.tsx
+
+import React from 'react'
+import SearchBar from './components/ui/searchBar' // Update the path to your SearchBar component
+
 import Play from './components/ui/play'
 
 export default function PlayPage() {
+  const handleSearch = (query: string) => {
+    // Implement your search logic here
+    console.log('Searching for:', query)
+  }
+
   return (
     <main className="h-screen">
+      {/* Include the SearchBar component */}
+      <SearchBar onSearch={handleSearch} />
+
+      {/* Other content on your page */}
       <Play />
     </main>
   )
