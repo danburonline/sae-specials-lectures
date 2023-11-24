75eb8db1bec36c70de4b1da80fa6466463670419
Gian-Carlo Delphis <g.delphis@outlook.com>
2023-11-24 15:11:30 +0100

    Update bun to 1.0.14 version

--- .devcontainer/scripts/post_create.sh
+++ .devcontainer/scripts/post_create.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install Bun as a faster drop-in replacement for NPM
-npm install -g bun@1.0.13
+npm install -g bun@1.0.14
 
 # Install dependencies via Bun from the Bun lockfile
 # ? The Bun lockfile is a binary file that contains a list of all dependencies
